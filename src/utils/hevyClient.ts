ba25b53241fc111c6d4eaf62f54a6dae2b82b5df
Christoph Kieslich <c.kieslich@gmail.com>
2025-03-26 10:47:11 +0100

    use builtin authentication provider

--- src/utils/hevyClient.ts
+++ src/utils/hevyClient.ts
@@ -1,24 +1,12 @@
-import type {
-	AuthenticationProvider,
-	RequestInformation,
+import {
+	ApiKeyAuthenticationProvider,
+	ApiKeyLocation,
 } from "@microsoft/kiota-abstractions";
 import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
 import { createHevyClient } from "../generated/client/hevyClient.js";
 
-class ApiKeyAuthProvider implements AuthenticationProvider {
-	private apiKey: string;
-
-	constructor(apiKey: string) {
-		this.apiKey = apiKey;
-	}
-
-	async authenticateRequest(request: RequestInformation): Promise<void> {
-		request.headers.add("x-api-key", this.apiKey);
-	}
-}
-
 export function createClient(apiKey: string, baseUrl: string) {
-	const authProvider = new ApiKeyAuthProvider(apiKey);
+	const authProvider = new ApiKeyAuthenticationProvider(apiKey, "api-key", ApiKeyLocation.Header);
 	const adapter = new FetchRequestAdapter(authProvider);
 	adapter.baseUrl = baseUrl;
 
